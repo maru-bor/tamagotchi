644360d14cce7f34d10b23e00ef30fdc6cbbc804
borisova <borisova.marie1@gmail.com>
2025-03-24 14:25:29 +0100

    Added component PetChoice.tsx

---
+++ src/PetChoice.tsx
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import catImg from './assets/cat.png';
+
+export default function PetChoice(){
+    return (
+        <>
+         <img src={catImg} alt="Pet cat"/>
+        </>
+
+    );
+}
