b90bdd12cb7f8afe16563c6972a6fbf711d4e3f3
borisova <borisova.marie1@gmail.com>
2025-03-24 14:35:52 +0100

    Added component PetChoice.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import './App.css'
 import PetChoice from "./PetChoice.tsx";
-
 function App() {
 
 
--- src/PetChoice.tsx
+++ src/PetChoice.tsx
@@ -1,10 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import catImg from './assets/cat.png';
+import dogImg from './assets/dog.png';
 
 export default function PetChoice(){
     return (
         <>
-         <img src={catImg} alt="Pet cat"/>
+            <img src={catImg} alt="Pet cat"/>
+            <img src={dogImg} alt="Pet dog"/>
         </>
 
     );
